5aa1bb040d6c6a0e95cfd098d401c416e08f55d6
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-10-25 15:28:50 +0500

    Changed

--- script/script.js
+++ script/script.js
@@ -1,8 +1,18 @@
-function showItemMenu(){
-    let firstItemMenu = document.getElementById("first");
-    if(firstItemMenu.style.display=="none"){
-        firstItemMenu.style.display="flex";
-    }else{
-        firstItemMenu.style.display="none";
+function showItemMenu(item){
+    if (item==='mainItem-1') {
+        let firstItemMenu = document.getElementById("first");
+        if(firstItemMenu.style.display=="none"){
+            firstItemMenu.style.display="flex";
+        }else{
+            firstItemMenu.style.display="none";
+        }    
+    }else if (item==='mainItem-2') {
+        let secondItemMenu = document.getElementById("second");
+        if(secondItemMenu.style.display=="none"){
+            secondItemMenu.style.display="flex";
+        }else{
+            secondItemMenu.style.display="none";
+        }    
     }
+    
 }
