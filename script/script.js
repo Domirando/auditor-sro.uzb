dd08583b2a879693d919a464cee4518913d9e95f
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-10-26 01:38:58 +0500

    Changed

--- script/script.js
+++ script/script.js
@@ -13,6 +13,48 @@ function showItemMenu(item){
         }else{
             secondItemMenu.style.display="none";
         }    
+    }else if (item==='mainItem-3') {
+        let thirdItemMenu = document.getElementById("third");
+        if(thirdItemMenu.style.display=="none"){
+            thirdItemMenu.style.display="flex";
+        }else{
+            thirdItemMenu.style.display="none";
+        }    
+    }else if (item==='mainItem-4') {
+        let fourthItemMenu = document.getElementById("fourth");
+        if(fourthItemMenu.style.display=="none"){
+            fourthItemMenu.style.display="flex";
+        }else{
+            fourthItemMenu.style.display="none";
+        }    
+    }else if (item==='mainItem-5') {
+        let fifthItemMenu = document.getElementById("fifth");
+        if(fifthItemMenu.style.display=="none"){
+            fifthItemMenu.style.display="flex";
+        }else{
+            fifthItemMenu.style.display="none";
+        }    
+    }else if (item==='mainItem-6') {
+        let sixthItemMenu = document.getElementById("sixth");
+        if(sixthItemMenu.style.display=="none"){
+            sixthItemMenu.style.display="flex";
+        }else{
+            sixthItemMenu.style.display="none";
+        }    
+    }else if (item==='mainItem-7') {
+        let seventhItemMenu = document.getElementById("seventh");
+        if(seventhItemMenu.style.display=="none"){
+            seventhItemMenu.style.display="flex";
+        }else{
+            seventhItemMenu.style.display="none";
+        }    
+    }else if (item==='mainItem-8') {
+        let eighthItemMenu = document.getElementById("eighth");
+        if(eighthItemMenu.style.display=="none"){
+            eighthItemMenu.style.display="flex";
+        }else{
+            eighthItemMenu.style.display="none";
+        }    
     }
     
 }
