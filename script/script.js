2164b6aa9e3cfe9a8586ccba2217cfdb453d101c
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-11-25 17:36:08 +0500

    Changing an event

--- script/script.js
+++ script/script.js
@@ -57,3 +57,62 @@ function showItemMenu(item) {
     }
   }
 }
+function hideItemMenu(item){
+  if (item === "mainItem-1") {
+    let firstItemMenu = document.getElementById("first");
+    if (firstItemMenu.style.display == "flex") {
+      firstItemMenu.style.display = "none";
+    } else {
+      firstItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-2") {
+    let secondItemMenu = document.getElementById("second");
+    if (secondItemMenu.style.display == "flex") {
+      secondItemMenu.style.display = "none";
+    } else {
+      secondItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-3") {
+    let thirdItemMenu = document.getElementById("third");
+    if (thirdItemMenu.style.display == "flex") {
+      thirdItemMenu.style.display = "none";
+    } else {
+      thirdItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-4") {
+    let fourthItemMenu = document.getElementById("fourth");
+    if (fourthItemMenu.style.display == "flex") {
+      fourthItemMenu.style.display = "none";
+    } else {
+      fourthItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-5") {
+    let fifthItemMenu = document.getElementById("fifth");
+    if (fifthItemMenu.style.display == "flex") {
+      fifthItemMenu.style.display = "none";
+    } else {
+      fifthItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-6") {
+    let sixthItemMenu = document.getElementById("sixth");
+    if (sixthItemMenu.style.display == "flex") {
+      sixthItemMenu.style.display = "none";
+    } else {
+      sixthItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-7") {
+    let seventhItemMenu = document.getElementById("seventh");
+    if (seventhItemMenu.style.display == "flex") {
+      seventhItemMenu.style.display = "none";
+    } else {
+      seventhItemMenu.style.display = "flex";
+    }
+  } else if (item === "mainItem-8") {
+    let eighthItemMenu = document.getElementById("eighth");
+    if (eighthItemMenu.style.display == "flex") {
+      eighthItemMenu.style.display = "none";
+    } else {
+      eighthItemMenu.style.display = "flex";
+    }
+  }
+}
