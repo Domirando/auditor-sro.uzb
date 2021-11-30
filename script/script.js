2e87bd1853f979e464a3e36fa9806e61015b684b
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-11-30 16:21:48 +0500

    Changes on js

--- script/script.js
+++ script/script.js
@@ -1,118 +1,133 @@
-function showItemMenu(item) {
-  if (item === "mainItem-1") {
-    let firstItemMenu = document.getElementById("first");
-    if (firstItemMenu.style.display == "none") {
-      firstItemMenu.style.display = "flex";
-    } else {
-      firstItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-2") {
-    let secondItemMenu = document.getElementById("second");
-    if (secondItemMenu.style.display == "none") {
-      secondItemMenu.style.display = "flex";
-    } else {
-      secondItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-3") {
-    let thirdItemMenu = document.getElementById("third");
-    if (thirdItemMenu.style.display == "none") {
-      thirdItemMenu.style.display = "flex";
-    } else {
-      thirdItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-4") {
-    let fourthItemMenu = document.getElementById("fourth");
-    if (fourthItemMenu.style.display == "none") {
-      fourthItemMenu.style.display = "flex";
-    } else {
-      fourthItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-5") {
-    let fifthItemMenu = document.getElementById("fifth");
-    if (fifthItemMenu.style.display == "none") {
-      fifthItemMenu.style.display = "flex";
-    } else {
-      fifthItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-6") {
-    let sixthItemMenu = document.getElementById("sixth");
-    if (sixthItemMenu.style.display == "none") {
-      sixthItemMenu.style.display = "flex";
-    } else {
-      sixthItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-7") {
-    let seventhItemMenu = document.getElementById("seventh");
-    if (seventhItemMenu.style.display == "none") {
-      seventhItemMenu.style.display = "flex";
-    } else {
-      seventhItemMenu.style.display = "none";
-    }
-  } else if (item === "mainItem-8") {
-    let eighthItemMenu = document.getElementById("eighth");
-    if (eighthItemMenu.style.display == "none") {
-      eighthItemMenu.style.display = "flex";
-    } else {
-      eighthItemMenu.style.display = "none";
-    }
-  }
+let menuContainers = document.querySelectorAll('.menuContainers');
+let itemsMenu = document.querySelectorAll('.itemsMenu');
+function mouseEnter(el){
+  // el.preventDefault();
+  itemsMenu[i].style.display = 'flex';
 }
-function hideItemMenu(item){
-  if (item === "mainItem-1") {
-    let firstItemMenu = document.getElementById("first");
-    if (firstItemMenu.style.display == "flex") {
-      firstItemMenu.style.display = "none";
-    } else {
-      firstItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-2") {
-    let secondItemMenu = document.getElementById("second");
-    if (secondItemMenu.style.display == "flex") {
-      secondItemMenu.style.display = "none";
-    } else {
-      secondItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-3") {
-    let thirdItemMenu = document.getElementById("third");
-    if (thirdItemMenu.style.display == "flex") {
-      thirdItemMenu.style.display = "none";
-    } else {
-      thirdItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-4") {
-    let fourthItemMenu = document.getElementById("fourth");
-    if (fourthItemMenu.style.display == "flex") {
-      fourthItemMenu.style.display = "none";
-    } else {
-      fourthItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-5") {
-    let fifthItemMenu = document.getElementById("fifth");
-    if (fifthItemMenu.style.display == "flex") {
-      fifthItemMenu.style.display = "none";
-    } else {
-      fifthItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-6") {
-    let sixthItemMenu = document.getElementById("sixth");
-    if (sixthItemMenu.style.display == "flex") {
-      sixthItemMenu.style.display = "none";
-    } else {
-      sixthItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-7") {
-    let seventhItemMenu = document.getElementById("seventh");
-    if (seventhItemMenu.style.display == "flex") {
-      seventhItemMenu.style.display = "none";
-    } else {
-      seventhItemMenu.style.display = "flex";
-    }
-  } else if (item === "mainItem-8") {
-    let eighthItemMenu = document.getElementById("eighth");
-    if (eighthItemMenu.style.display == "flex") {
-      eighthItemMenu.style.display = "none";
-    } else {
-      eighthItemMenu.style.display = "flex";
-    }
-  }
+function mouseEnter(el){
+  // el.preventDefault();
+  itemsMenu[i].style.display = 'none';
 }
+for(let i = 0; i<=menuContainers.length-1; i++){
+  menuContainers[i].addEventListener("onmouseenter", mouseEnter(i));
+    menuContainers[i].addEventListener("onmouseleave", mouseLeave(i));
+}
+
+// function showItemMenu(item) {
+//   if (item === "mainItem-1") {
+//     let firstItemMenu = document.getElementById("first");
+//     if (firstItemMenu.style.display == "none") {
+//       firstItemMenu.style.display = "flex";
+//     } else {
+//       firstItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-2") {
+//     let secondItemMenu = document.getElementById("second");
+//     if (secondItemMenu.style.display == "none") {
+//       secondItemMenu.style.display = "flex";
+//     } else {
+//       secondItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-3") {
+//     let thirdItemMenu = document.getElementById("third");
+//     if (thirdItemMenu.style.display == "none") {
+//       thirdItemMenu.style.display = "flex";
+//     } else {
+//       thirdItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-4") {
+//     let fourthItemMenu = document.getElementById("fourth");
+//     if (fourthItemMenu.style.display == "none") {
+//       fourthItemMenu.style.display = "flex";
+//     } else {
+//       fourthItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-5") {
+//     let fifthItemMenu = document.getElementById("fifth");
+//     if (fifthItemMenu.style.display == "none") {
+//       fifthItemMenu.style.display = "flex";
+//     } else {
+//       fifthItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-6") {
+//     let sixthItemMenu = document.getElementById("sixth");
+//     if (sixthItemMenu.style.display == "none") {
+//       sixthItemMenu.style.display = "flex";
+//     } else {
+//       sixthItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-7") {
+//     let seventhItemMenu = document.getElementById("seventh");
+//     if (seventhItemMenu.style.display == "none") {
+//       seventhItemMenu.style.display = "flex";
+//     } else {
+//       seventhItemMenu.style.display = "none";
+//     }
+//   } else if (item === "mainItem-8") {
+//     let eighthItemMenu = document.getElementById("eighth");
+//     if (eighthItemMenu.style.display == "none") {
+//       eighthItemMenu.style.display = "flex";
+//     } else {
+//       eighthItemMenu.style.display = "none";
+//     }
+//   }
+// }
+// function hideItemMenu(item){
+//   if (item === "mainItem-1") {
+//     let firstItemMenu = document.getElementById("first");
+//     if (firstItemMenu.style.display == "flex") {
+//       firstItemMenu.style.display = "none";
+//     } else {
+//       firstItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-2") {
+//     let secondItemMenu = document.getElementById("second");
+//     if (secondItemMenu.style.display == "flex") {
+//       secondItemMenu.style.display = "none";
+//     } else {
+//       secondItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-3") {
+//     let thirdItemMenu = document.getElementById("third");
+//     if (thirdItemMenu.style.display == "flex") {
+//       thirdItemMenu.style.display = "none";
+//     } else {
+//       thirdItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-4") {
+//     let fourthItemMenu = document.getElementById("fourth");
+//     if (fourthItemMenu.style.display == "flex") {
+//       fourthItemMenu.style.display = "none";
+//     } else {
+//       fourthItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-5") {
+//     let fifthItemMenu = document.getElementById("fifth");
+//     if (fifthItemMenu.style.display == "flex") {
+//       fifthItemMenu.style.display = "none";
+//     } else {
+//       fifthItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-6") {
+//     let sixthItemMenu = document.getElementById("sixth");
+//     if (sixthItemMenu.style.display == "flex") {
+//       sixthItemMenu.style.display = "none";
+//     } else {
+//       sixthItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-7") {
+//     let seventhItemMenu = document.getElementById("seventh");
+//     if (seventhItemMenu.style.display == "flex") {
+//       seventhItemMenu.style.display = "none";
+//     } else {
+//       seventhItemMenu.style.display = "flex";
+//     }
+//   } else if (item === "mainItem-8") {
+//     let eighthItemMenu = document.getElementById("eighth");
+//     if (eighthItemMenu.style.display == "flex") {
+//       eighthItemMenu.style.display = "none";
+//     } else {
+//       eighthItemMenu.style.display = "flex";
+//     }
+//   }
+// }
