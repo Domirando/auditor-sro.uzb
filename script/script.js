96b5a156be75941ef40f497532f2e40296364b72
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-12-03 05:56:43 +0500

    Just spacing

--- script/script.js
+++ script/script.js
@@ -3,11 +3,11 @@ var itemsMenu = document.querySelectorAll('.itemsMenu');
 var menuCond = false;
 
 for (let x = menuContainers.length - 1; x >= 0; x--){
-    menuContainers[x].onmouseenter = ()=>{
+    menuContainers[x].onmouseenter = () => {
       itemsMenu[x].classList.add("flex");  
       itemsMenu[x].classList.remove("none");  
     };
-    menuContainers[x].onmouseleave = ()=>{
+    menuContainers[x].onmouseleave = () => {
       itemsMenu[x].classList.add("none");  
       itemsMenu[x].classList.remove("flex");  
     };
